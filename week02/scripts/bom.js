d017cb765d39aa59de43577098e271d525a7aa76
paternehountondji <phountondji@byupathway.edu>
2026-07-10 04:06:44 +0100

    W02 DOM events activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,12 +2,26 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener('click', function() {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-deleteButton.setAttribute('aria-label', 'Remove chapter');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', 'Remove ' + input.value);
 
-li.append(deleteButton);
-list.append(li);
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = '';
+    input.focus();
+  } else {
+    input.focus();
+  }
+});
